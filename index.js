c0d3bde459b7731dfc35ea125bd93fe1bda98b31
Taylor Ng <taylormng95@gmail.com>
2024-05-15 15:48:33 -0400

    completes acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+const dodger = document.querySelector('#dodger');
+
+document.addEventListener('keydown', function(event){
+    if(event.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    }
+})
+
+function moveDodgerLeft(){
+    const leftPos = dodger.style.left.replace('px', '');
+    const leftPosNum = parseInt(leftPos, 10);
+    if(leftPosNum > 0){
+        dodger.style.left = `${leftPosNum - 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(event){
+    if(event.key === 'ArrowRight'){
+        moveDodgerRight();
+    }
+})
+
+function moveDodgerRight(){
+    const rightPos = dodger.style.left.replace('px', '');
+    const rightPosNum = parseInt(rightPos, 10);
+    console.log(rightPosNum);
+    if(rightPosNum < 360){
+        dodger.style.left = `${rightPosNum + 1}px`;
+    }
+}
